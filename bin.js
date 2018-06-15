279d81aad4a3fa752c26449b9e62a6b69fe7cc0a
Renée Kooi <renee@kooi.me>
2018-06-15 10:27:18 +0200

    Throw on older Nodes

--- bin.js
+++ bin.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+var major = /^v(\d+)\./.exec(process.version)
+if (parseInt(major[1], 10) < 10) {
+  throw new Error('esm-bundler requires Node 10')
+}
+
 var path = require('path')
 var spawn = require('child_process').spawnSync
 
